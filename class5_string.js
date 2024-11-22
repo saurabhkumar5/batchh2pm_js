ce0229514db3bf2e123209e371cbb2d90f24899e
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-22 14:16:27 +0530

    string

--- class5_string.js
+++ class5_string.js
@@ -2,6 +2,79 @@
 // console.log(typeof(a))
 
 
-let a = "anmol"
+// let a = "anmol"
 // console.log(a.length)
-console.log(a[3])
+// console.log(a[3])
+
+
+// 1. concat()
+
+// let a = "raman"
+// let b = "komal"
+// // console.log(a+"      "+b)
+// console.log(a.concat(b))
+
+
+// 2. indexOf()
+
+//   let a = "komal"
+//   console.log(a.indexOf('l'))
+
+// 3. replace()
+
+//   let a = "welcome";
+//   console.log(a.replace('welcome','most'))
+
+// 4. search()
+
+        // let a = "welcome"
+        // console.log(a.search('come'))
+
+
+// 5. toLowerCase()   
+
+    //  let a = "sonam"
+    //  console.log(a.toLowerCase())
+    // console.log(a.toUpperCase())
+
+        //  console.log(a[0].toUpperCase()+a)
+
+
+//   6. includes()   
+
+
+    // let a = "most welcome"
+    // console.log(a.includes('wel'))
+
+
+    // 7. trim()
+
+
+    //   let a = "                sonam                 "
+    //   console.log(a.trim()+"komal")
+
+
+    // 8. startwidth
+    //   let a = "most welcome";
+    //   console.log(a.startsWith('welcome'))
+    // console.log(a.endsWith('come'))
+
+    // 9. charAt()
+
+    //    let a = "anmol"
+    //    console.log(a.charAt(2))
+
+    // 10.split()
+
+    // let a = "anuj neeraj";
+    //     console.log(a.split(' '))
+
+
+    // 11. repeat()
+
+    //   let a = "neeraj"
+    //   console.log(a.repeat(3))
+
+
+    
+      
